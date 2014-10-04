a88673f6bb6506496f26b32322786955a447a6be
francisco_maciel <francisco.ma.maciel@gmail.com>
2014-10-04 19:15:35 +0100

    Added  json requests body parser

--- src/server/server.js
+++ src/server/server.js
@@ -13,10 +13,15 @@ var bodyParser = require('body-parser')
 //Configuration
 app.use(express.static(__dirname + '/public'));
 app.use(morgan('dev'));
-app.use(bodyParser({
+
+
+app.use(bodyParser.json({ type: ['json','text/*'] }));
+/*
+app.use(bodyParser.json({
     maxBodySize: 65535,
     mapParams: false
-}));
+}));*/
+
 
 app.set('port', port);
 
