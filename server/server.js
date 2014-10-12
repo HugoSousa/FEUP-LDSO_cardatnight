fc62e11d88e0bec12d9dbfeb8665bbb08d90cf37
Francisco Maciel <francisco.ma.maciel@gmail.com>
2014-10-12 16:22:23 +0100

    fixed server headers for cross-domain

--- server/server.js
+++ server/server.js
@@ -11,11 +11,31 @@ var io = require('socket.io')(http);
 var bodyParser = require('body-parser');
 var passport = require('passport');
 app.use(morgan('dev'));
-app.all('*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
- });
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.header('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, content-type, x-access-token');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.header('Access-Control-Allow-Credentials', true);
+    
+    if (req.method === 'OPTIONS') {
+        // Bypass browser's CORS options requests
+        res.send(200);
+    } else {
+        // Pass to next layer of middleware
+        next();
+    }
+});
+
 app.use(passport.initialize());
 var session = require('express-session');
 require('./routes/modules/passport')(passport);
