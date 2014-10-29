699f4d13a837d490052bc0dde349b67e843c2736
francisco_maciel <francisco.ma.maciel@gmail.com>
2014-10-29 11:40:39 +0000

    fixed server transaction on register

--- server/routes/modules/database.js
+++ server/routes/modules/database.js
@@ -5,36 +5,51 @@ var database_url = process.env.DATABASE_URL || 'postgres://udkegzydszxlfg:abUNsy
 
 exports.addCustomer = function (name, email , username , password, callback)
 {
+var rollback = function(client, done, callback, error, result) {
+    client.query('ROLLBACK',function(err) {
+        if (err) error = 'database error on rollback';
+        callback(error,result);
+        return done(err);
+       });
+};
  //Improvement to do: encapsulate in transaction
     pg.connect(database_url , function (err, client, done) {
         if (err) {
             callback('Failed to connecto do database' , null);
         }
         else {
-            
-            client.query({ text: "INSERT INTO person(username,password) VALUES( $1, $2) RETURNING personId", name: 'insert_person', values: [username,password] }, function (err, result) {
-                if (err) {
-                    if (err.code == 23505) //username already exists (unique key constraint code)
-                        callback("Username already exists" , null);
-                    else callback(err, null);
-                }
+            client.query('BEGIN', function (err) {
+                if (err) return rollback(client,done, callback, "Unknown database error. Try again later." , null);
                 else {
-
-                    var id = result.rows[0].personid;
-                    
-                    client.query({ text: "INSERT INTO customer(customerId,name,email) VALUES( $1, $2, $3)", name: 'insert_customer', values: [id, name, email] }, function (err, result) {
-                         if (err) {
-
-								if (err.code == 23505) //username already exists (unique key constraint code)
-									callback("Email already exists" , null);
-								else callback(err, null);
-							}
+                
+                    client.query({ text: "INSERT INTO person(username,password) VALUES( $1, $2) RETURNING personId", name: 'insert_person', values: [username,password] }, function (err, result) {
+                        if (err) {
+                            if (err.code == 23505) //username already exists (unique key constraint code)
+                                rollback(client,done, callback,"Username already exists" , null);
+                            else rollback(client,done, callback,err, null);
+                        }
                         else {
-                            callback(null, { name: name, email: email, username: username, password: password });
+                            process.nextTick(function() {
+                                var id = result.rows[0].personid;
+                                
+                                client.query({ text: "INSERT INTO customer(customerId,name,email) VALUES( $1, $2, $3)", name: 'insert_customer', values: [id, name, email] }, function (err, result) {
+                                     if (err) {
+
+                                            if (err.code == 23505) //username already exists (unique key constraint code)
+                                                rollback(client,done,callback,"Email already exists", null);
+                                            else  rollback(client,done,callback,err, null);
+
+                                        }
+                                    else {
+                                        client.query('COMMIT', done);
+                                        callback(null, { name: name, email: email, username: username, password: password });
+
+                                    }
+                                });
+                            });
 
                         }
                     });
-
                 }
             });
         }
