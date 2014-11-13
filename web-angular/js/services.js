d0d026efa13fb3b27c41a4b91a1e369bd915f1fa
ruigames <rui_games@hotmail.com>
2014-11-13 14:15:32 +0000

    Alerts when product is successfully added/edited/deleted

--- web-angular/js/services.js
+++ web-angular/js/services.js
@@ -25,3 +25,30 @@ app.factory('AuthService', function(){
     }
 
 });
+
+app.factory('alertService', [
+      '$rootScope', function($rootScope) {
+        var alertService;
+        $rootScope.alerts = [];
+        return alertService = {
+          add: function(type, msg) {
+            return $rootScope.alerts.push({
+              type: type,
+              msg: msg,
+              close: function() {
+                return alertService.closeAlert(this);
+              }
+            });
+          },
+          closeAlert: function(alert) {
+            return this.closeAlertIdx($rootScope.alerts.indexOf(alert));
+          },
+          closeAlertIdx: function(index) {
+            return $rootScope.alerts.splice(index, 1);
+          },
+          clear: function(){
+            $rootScope.alerts = [];
+          }
+        };
+      }
+    ])
